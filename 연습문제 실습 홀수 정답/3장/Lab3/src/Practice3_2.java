7c3f927076852bbe9aa335e621dc6d81430c45bc
cha youn sung <vaite714@gmail.com>
2020-09-16 20:15:27 +0900

    Add Lab 3

---
+++ 연습문제 실습 홀수 정답/3장/Lab3/src/Practice3_2.java
@@ -0,0 +1,37 @@
+public class Practice3_2 {
+	public static void main (String[] args) {
+		int intArray[][] = new int[4][4];
+
+		
+		// 2차원 배열의 모든 원소를 0으로 초기화 
+		for(int i = 0; i < 4; i++)
+		{
+			for(int j = 0; j < 4; j++)
+			{
+				intArray[i][j] = 0;
+			}
+		}
+		
+		// 배열의 9개 랜덤위치에 랜덤한 정수 생성하여 저장
+		
+		for(int i = 0; i < 4; i++)
+		{
+			for(int j = 0; j < 4; j++)
+			{
+				intArray[i][j] = (int)(Math.random()*9 + 1);
+			}
+		}
+
+		
+		// 2차원 배열 출력
+		System.out.println("---- Random Matrix ----");
+		for(int i = 0; i < 4; i++)
+		{
+			for(int j = 0; j < 4; j++)
+			{
+				System.out.print(intArray[i][j] + " ");
+			}
+			System.out.println(" ");
+		}		
+	}
+}
